64415e89672d2efc506d22c193eee98ed26a288e
Daniel David Marques de Castro <ist187644@bagaco.scdeec.tecnico.ulisboa.pt>
2019-12-09 20:50:03 +0000

    Key exchange and encryption of the messages concluded

--- MP/mp_bridge.py
+++ MP/mp_bridge.py
@@ -9,6 +9,9 @@ from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives.serialization import Encoding
 from cryptography.hazmat.primitives.serialization import PublicFormat
 
+from cryptography.fernet import Fernet
+import base64
+
 
 logging.basicConfig(
     format='%(name)s [%(levelname)s]\t%(asctime)s - %(message)s',
@@ -18,6 +21,7 @@ logging.basicConfig(
 logger = logging.getLogger('MP_Bridge')
 
 d = {"anakin":["brakes"], "zeus":["oil"]} # d = {"anakin":["radio"], "zeus":["brakes", "oil"]}
+f = None
 
 
 class MP_Bridge(socketserver.BaseRequestHandler):
@@ -33,7 +37,7 @@ class MP_Bridge(socketserver.BaseRequestHandler):
         """
         logger.info("Connect to zeus and anakin")
         #self.zeus = socket.create_connection(('localhost',5679))
-        self.anakin = socket.create_connection(('localhost',5679))
+        self.anakin = socket.create_connection(('localhost',5680))
         logger.info("Connected")
 
         #self.zeus = socket.create_connection(('zeus',5679))
@@ -44,17 +48,13 @@ class MP_Bridge(socketserver.BaseRequestHandler):
         # self.request is the TCP socket connected to the client
         logger.debug("{}: start handling requests".format(self.client_address[0]))
 
+        firstMsg = True
+
         serve = True
         while serve:
-            # raw_request is encrypted with the session key
-            raw_request = self.request.recv(49)
-            testing_v = True
-            
-            # I have to decrypt with the session key, Im Heimdall
-
-
-            if(testing_v):
-                logger.info("{}: Received key {}".format(self.client_address[0], raw_request))
+            if(firstMsg):
+                raw_request = self.request.recv(49)
+                #logger.info("{}: Received key {}".format(self.client_address[0], raw_request))
 
                 # Generate a private key for use in the exchange.
                 server_private_key = ec.generate_private_key(
@@ -62,58 +62,67 @@ class MP_Bridge(socketserver.BaseRequestHandler):
                 )
                 server_public_key = server_private_key.public_key()
 
-                # In a real handshake the peer is a remote client. For this
-                # example we'll generate another local private key though. Note that in
-                # a DH handshake both peers must agree on a common set of parameters.
+                # Get client public key from the socket.
                 client_public_key = ec.EllipticCurvePublicKey.from_encoded_point(ec.SECP384R1(), raw_request)
 
                 shared_key = server_private_key.exchange(ec.ECDH(), client_public_key)
-                # Perform key derivation.
-                derived_key = HKDF(algorithm=hashes.SHA256(),length=32,salt=None,info=b'handshake data',backend=default_backend()).derive(shared_key)
 
+                # Perform key derivation. This is the session key.
+                session_key = HKDF(algorithm=hashes.SHA256(),length=32,salt=None,info=b'handshake data',backend=default_backend()).derive(shared_key)
+
+                # Send key to the sender.
                 self.request.sendall(server_public_key.public_bytes(encoding=Encoding.X962, format=PublicFormat.CompressedPoint))
 
 
-            else:
-                logger.debug("{}: request {} is {}".format(self.client_address[0],
-                                                            raw_request,
-                                                            rest))
+                encoded_s_key = base64.b64encode(session_key)
+                f = Fernet(encoded_s_key)
 
-                logger.info("{}: Got request {} for {}".format(self.client_address[0],
-                                                                rest,
-                                                                destination.getpeername()))
+                firstMsg = False
+
+
+
+
+            else:
+                raw_request = self.request.recv(256)
+                #logger.info("{}: Got request {} for {}".format(self.client_address[0],
+                #                                                rest,
+                #                                                destination.getpeername()))
+
+                # DECRYPT
+                try:
+                    msg = f.decrypt(raw_request)
+                except:
+                    logger.warning("Invalid Token")
+                    return
+
+                logger.info("{}: raw_request {} is {}".format(self.client_address[0],
+                                                                raw_request,
+                                                                msg))
+                destination, rest = self.sanitize(msg)
 
                 # send to destination the decrypted request
                 destination.sendall(bytes(rest, 'utf-8'))
                 logger.info("{}: Forwarded request".format(self.client_address[0]))
+
                 # receive response (for example motor levels)
                 response = destination.recv(256)
                 logger.info("{}: Forwarding response {}".format(self.client_address[0],
                                                                 response))
+                # ENCRYPT
+                response = f.encrypt(response)
                 self.request.sendall(response)
 
 
     def sanitize(self, to_sanitize):
-    # TODO: bullet-proof this
-
         try:
-            msg = to_sanitize.decode('utf-8')
-            dest, rest = to_sanitize.decode('utf-8').strip().split('|', 1)
+            dest, rest = to_sanitize.decode('ascii').strip().split('|', 1)
 
-
-            # Logic for getting the destination with the command
-            if(dest == "key"):
-                return dest, rest
-            elif(dest in d["anakin"]):
+            if dest == 'anakin':
                 dest = self.anakin
-            elif(dest in d["zeus"]):
-                print("t1")
-                dest = self.zeus
             else:
-                print("t")
-                raise ValueError("Invalid user command: wrong parameter") # bad exception
+                dest = self.anakin #self.zeus
 
-            return dest, msg
+            return dest, rest
 
         except:
             raise ValueError("Invalid user command")
@@ -127,9 +136,9 @@ class MP_Bridge(socketserver.BaseRequestHandler):
         self.anakin.close()
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 5678
+    HOST, PORT = "localhost", 5555
 
-    # Create the server, binding to localhost on port 9999
+    # Create the server, binding to localhost on port 5555
     with socketserver.TCPServer((HOST, PORT), MP_Bridge) as server:
         # Activate the server; this will keep running until you
         # interrupt the program with Ctrl-C
--- MP/mp_client.py
+++ MP/mp_client.py
@@ -8,62 +8,51 @@ from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives.serialization import Encoding
 from cryptography.hazmat.primitives.serialization import PublicFormat
 
-HOST, PORT = "localhost", 5678
+from cryptography.fernet import Fernet
+import base64
+
+HOST, PORT = "localhost", 5555
 data = " ".join(sys.argv[1:])
 
 
-def exchange_skey():
-	private_key = ec.generate_private_key(
-	    ec.SECP384R1(), default_backend()
-	)
-	# Generate a private key for use in the exchange.
-	server_private_key = parameters.generate_private_key()
-
-	#COMMUNICATE to get sahred key
-	#...
-	#pub_key = ...
-
-	shared_key = server_private_key.exchange(pub_key) # shared_key = server_private_key.exchange(peer_private_key.public_key())
-
-	derived_key = HKDF(
-	    algorithm=hashes.SHA256(),
-	    length=32,
-	    salt=None,
-	    info=b'handshake data',
-	    backend=default_backend()
-	).derive(shared_key)
-
-	return shared_key
-
-
 if __name__ == "__main__":
-	# Generate a private key for use in the exchange.
-	client_private_key = ec.generate_private_key(
-    	ec.SECP384R1(), default_backend()
-	)
-	client_public_key = client_private_key.public_key()
-	print(client_private_key)
 
-
-    # Create a socket (SOCK_STREAM means a TCP socket)
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-	    # Connect to server and send data
-	    sock.connect((HOST, PORT))
+		sock.connect((HOST, PORT))
+		# EXCHANGE SESSION KEY
+		# Generate a private key for use in the exchange.
+		client_private_key = ec.generate_private_key(
+	    	ec.SECP384R1(), default_backend()
+		)
+		client_public_key = client_private_key.public_key()
 
-	    print(len(client_public_key.public_bytes(encoding=Encoding.X962, format=PublicFormat.CompressedPoint)))
-	    sock.sendall(client_public_key.public_bytes(encoding=Encoding.X962, format=PublicFormat.CompressedPoint))
-	    #key_response = str(sock.recv(4096), "utf-8")
-	    server_public_key = sock.recv(49)
-	    server_public_key = ec.EllipticCurvePublicKey.from_encoded_point(ec.SECP384R1(), server_public_key)
-	    #key_response.decode('utf-8').strip().split('|', 1)
-	    shared_key = client_private_key.exchange(ec.ECDH(), server_public_key)
-	    session_key = HKDF(algorithm=hashes.SHA256(),length=32,salt=None,info=b'handshake data',backend=default_backend()).derive(shared_key)
+		# Send the public key to the bridge
+		sock.sendall(client_public_key.public_bytes(encoding=Encoding.X962, format=PublicFormat.CompressedPoint))
 
-	    """encMsg = data #sign.data with ks, etc...
-	    sock.sendall(bytes(data, "utf-8"))
+		# Receive the public key from bridge
+		server_public_key = sock.recv(49)
+		server_public_key = ec.EllipticCurvePublicKey.from_encoded_point(ec.SECP384R1(), server_public_key)
+		#session_key_2 = sock.recv(1024)
 
-		# Receive data from the server and shut down
-	    received = str(sock.recv(1024), "utf-8")
+		# Generate the session key
+		shared_key = client_private_key.exchange(ec.ECDH(), server_public_key)
+		session_key = HKDF(algorithm=hashes.SHA256(),length=32,salt=None,info=b'handshake data',backend=default_backend()).derive(shared_key)
+		# EXCHANGE SESSION KEY
+		#print(session_key == session_key_2)
+
+		encoded_s_key = base64.b64encode(session_key)
+		f = Fernet(encoded_s_key)
+		token = f.encrypt(bytes(data + "\n", "utf-8")) # b"anakin|brakes"
+		sock.sendall(token)
+
+		response = sock.recv(1024)
+		try:
+			response = f.decrypt(response)
+		except:
+			print("Invalid Token")
+		print(response)
+
+
 
 	print("Sent:     {}".format(data))
-	print("Received: {}".format(received))"""
+	print("Received: {}".format(response))
